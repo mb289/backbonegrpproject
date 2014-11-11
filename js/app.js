c679eab59c6ef18b51a1df4137ad24cfc4741d4d
Julie Rich <julierich1211@yahoo.com>
2014-11-11 11:33:26 -0600

    backbone start

--- js/app.js
+++ js/app.js
@@ -1,37 +1,24 @@
 //https://www.googleapis.com/books/v1/volumes?q=search+terms:keyes&key=AIzaSyBU9KgSbBKQMno6QgEoB75TPSRN1c16fLI
-    window.onload = app;
+window.onload = app;
 
-    // runs when the DOM is loaded
-    function app(){
+// runs when the DOM is loaded
+function app() {
 
-        // load some scripts (uses promises :D)
-        loader.load(
-            {url: "./bower_components/jquery/dist/jquery.min.js"},
-            {url: "./bower_components/lodash/dist/lodash.min.js"},
-            {url: "./bower_components/backbone/backbone.js"},
-            {url: "./bower_components/pathjs/path.min.js"}
-        ).then(function(){
-            _.templateSettings.interpolate = /{([\s\S]+?)}/g;
+    // load some scripts 
+    loader.load({
+        url: "./bower_components/jquery/dist/jquery.min.js"
+    }, {
+        url: "./bower_components/lodash/dist/lodash.min.js"
+    }, {
+        url: "./bower_components/backbone/backbone.js"
+    }, {
+        url: "./bower_components/pathjs/path.min.js"
+    }, {
+        url: "./js/views/book.js"
+    }).then(function() {
+        _.templateSettings.interpolate = /{([\s\S]+?)}/g;
+        var book = new GoogleBook();
+        //start app?
+    })
 
-            // start app?
-        })
-
-    }
-    
-
-var app = app || {};
-
-$(function() {
-    var books = [
-        { title: 'JavaScript: The Good Parts', author: 'Douglas Crockford', releaseDate: '2008', keywords: 'JavaScript Programming' },
-        { title: 'The Little Book on CoffeeScript', author: 'Alex MacCaw', releaseDate: '2012', keywords: 'CoffeeScript Programming' },
-        { title: 'Scala for the Impatient', author: 'Cay S. Horstmann', releaseDate: '2012', keywords: 'Scala Programming' },
-        { title: 'American Psycho', author: 'Bret Easton Ellis', releaseDate: '1991', keywords: 'Novel Splatter' },
-        { title: 'Eloquent JavaScript', author: 'Marijn Haverbeke', releaseDate: '2011', keywords: 'JavaScript Programming' }
-    ];
-
-    new app.LibraryView( books );
-});
-//Our app just passes the sample data to a new instance of app.LibraryView that it creates. Since the initialize() constructor in LibraryView 
-//invokes the view’s render() method, all the books in the library will be displayed. Since we are passing our entry point as a callback to jQuery 
-//(in the form of its $ alias), the function will execute when the DOM is ready.
+}
