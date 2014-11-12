fc276950d15c0dfae50b1b58fa7c7274ac26905a
Adam Bergen <bergen@gmail.com>
2014-11-12 10:12:09 -0600

    got input object working

--- js/views/book.js
+++ js/views/book.js
@@ -1,6 +1,14 @@
 (function(window, undefined) {
     //"use strict";
 
+    function GoogleBook() {
+         
+        this.bookCollection = new BookListings();
+        
+        this.Routing();
+
+        }
+
     var containerView = Backbone.View.extend({
         tagName: "div",
         idName: "container",
@@ -21,67 +29,54 @@
         }
     });
 
-    function GoogleBook(options) {
-            this.options = _.extend({}, options, {
-                key: "AIzaSyBU9KgSbBKQMno6QgEoB75TPSRN1c16fLI",
-            });
-            this.Routing();
+    var BookListing = Backbone.Model.extend({
+        initialize: function(){
+            this.view = new containerView({ model: this 
+                });
         }
-        //https://www.googleapis.com/books/v1/volumes?q=search+terms:keyes&key=AIzaSyBU9KgSbBKQMno6QgEoB75TPSRN1c16fLI
-    
-    GoogleBook.prototype.createInputObject = function(){
+    });
 
-        var input = {};
-        $(':input').each(function(){
-            input[this.name] = this.value;
-        });
-        console.log(input);
-        return input;
-    }
+    var BookListings = Backbone.Collection.extend({
 
-    GoogleBook.prototype.queryAPI = function() {
-        
-        var input = this.createInputObject();
+        createInputObject: function(){
+            var input = {};
+            $(':input').each(function(){
+                input[this.name] = this.value;
+            })
+            return input;
+        },
 
-        var url = [
-            "https://www.googleapis.com/books/v1/volumes",
-            "?q=",
-            input.genre,
+        model: BookListing,
+
+        apikey: "AIzaSyBU9KgSbBKQMno6QgEoB75TPSRN1c16fLI",
+
+        url: function(){
+
+            return [
+            "https://www.googleapis.com/books/v1/volumes?q=",
+            this.createInputObject().genre,
             "&key=",
-            this.options.key,
-        ].join('');
-        
-        console.log(url);
-        
-        return $.get(url).then(function(data) {
-            console.log(data);
-            return data;
-        });
-    };
-    GoogleBook.prototype.makeGoogleBookRequest = function(data) {
-        $.when(
-            this.queryAPI(data)
-        ).then(function(data) {
-            console.log(data);
-            //debugger;
-            if (!(data.items instanceof Array)
+            this.apikey
+            ].join('');
+        },
 
-            ) {
-                throw new Error("Shit Doesn't Work!");
-            }
-            data.items.forEach(function(data) {
-                new containerView({ model: data});
-            });
-        });
-    };
-    
+        parse: function(data){
+            return data.items;
+        },
+
+    });
+
+    // var bookRouting = Backbone.Routing.extend({
+
+    // })
+
     GoogleBook.prototype.Routing = function() {
         
         var self = this;
 
         Path.map("#/results").to(function(){
 
-            self.makeGoogleBookRequest();
+            self.bookCollection.fetch();
 
     });
 
@@ -93,3 +88,56 @@
     window.GoogleBook = GoogleBook;
 
 })(window, undefined);
+
+
+    
+        //https://www.googleapis.com/books/v1/volumes?q=search+terms:keyes&key=AIzaSyBU9KgSbBKQMno6QgEoB75TPSRN1c16fLI
+    
+    // GoogleBook.prototype.createInputObject = function(){
+
+    //     var input = {};
+    //     $(':input').each(function(){
+    //         input[this.name] = this.value;
+    //     });
+    //     console.log(input);
+    //     return input;
+    // }
+
+    // GoogleBook.prototype.queryAPI = function() {
+        
+    //     var input = this.createInputObject();
+
+    //     var url = [
+    //         "https://www.googleapis.com/books/v1/volumes",
+    //         "?q=",
+    //         input.genre,
+    //         "&key=",
+    //         this.options.key,
+    //     ].join('');
+        
+    //     console.log(url);
+        
+    //     return $.get(url).then(function(data) {
+    //         console.log(data);
+    //         return data;
+    //     });
+    // };
+    
+    // GoogleBook.prototype.makeGoogleBookRequest = function(data) {
+    //     $.when(
+    //         this.queryAPI(data)
+    //     ).then(function(data) {
+    //         console.log(data);
+    //         //debugger;
+    //         if (!(data.items instanceof Array)
+
+    //         ) {
+    //             throw new Error("Shit Doesn't Work!");
+    //         }
+    //         data.items.forEach(function(data) {
+    //             new containerView({ model: data});
+    //         });
+    //     });
+    // };
+    
+
