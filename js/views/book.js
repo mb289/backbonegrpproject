141ad041935cda4e706efe28e03bd3bdcaed6331
Adam Bergen <bergen@gmail.com>
2014-11-12 11:50:04 -0600

    event handle input, got it working with matts help

--- js/views/book.js
+++ js/views/book.js
@@ -2,14 +2,10 @@
     //"use strict";
 
     function GoogleBook() {
-         
-        this.bookCollection = new BookListings();
-        
-        this.Routing();
+        this.Routing = new bookRouting();
+    }
 
-        }
-
-    var containerView = Backbone.View.extend({
+    var ContainerView = Backbone.View.extend({
         tagName: "div",
         idName: "container",
         initialize: function(opts) {
@@ -24,23 +20,23 @@
         },
         template: "<img src={volumeInfo.imageLinks.thumbnail}/>",
         render: function() {
-    
             this.el.innerHTML = _.template(this.template, this.model.attributes);
         }
     });
 
     var BookListing = Backbone.Model.extend({
-        initialize: function(){
-            this.view = new containerView({ model: this 
-                });
+        initialize: function() {
+            this.view = new ContainerView({
+                model: this
+            });
         }
     });
 
     var BookListings = Backbone.Collection.extend({
 
-        createInputObject: function(){
+        createInputObject: function() {
             var input = {};
-            $(':input').each(function(){
+            $(':input').each(function() {
                 input[this.name] = this.value;
             })
             return input;
@@ -50,94 +46,60 @@
 
         apikey: "AIzaSyBU9KgSbBKQMno6QgEoB75TPSRN1c16fLI",
 
-        url: function(){
+        url: function() {
             return [
-            "https://www.googleapis.com/books/v1/volumes?q=",
-            this.createInputObject().genre,
-            "&key=",
-            this.apikey
+                "https://www.googleapis.com/books/v1/volumes?q=",
+                this.createInputObject().genre,
+                "&key=",
+                this.apikey
             ].join('');
         },
 
-        parse: function(data){
+        parse: function(data) {
             console.log(data);
             return data.items;
         },
 
-    });
-
-    // var bookRouting = Backbone.Routing.extend({
-
-    // })
-
-    GoogleBook.prototype.Routing = function() {
-        
-        var self = this;
-
-        Path.map("#/results").to(function(){
-
-            self.bookCollection.fetch();
+        startRequests: function(){
+            if(this.createInputObject().genre){
+                return this.fetch();
+            }
+        }
 
     });
 
-        Path.root("#/")
-        Path.listen();
-};
+    var AppView = Backbone.View.extend({
+        el: document.querySelector('body'),
+        initialize: function(){
+            this.bookCollection = new BookListings();
+        },
+        events: {
+            "submit form": "search"
+        },
+        search: function(event){
+            event.preventDefault();
+            this.bookCollection.startRequests();
+        }
+    })
 
+    var bookRouting = Backbone.Router.extend({
+
+        initialize: function() {
+            this.appView = new AppView();
+            Backbone.history.start();
+        },
+
+        routes: {
+            "*anything": "showResults"
+        },
+
+        showResults: function() {
+            // access the value of the input, if not empty then fetch
+            this.appView.bookCollection.startRequests();
+        }
+
+    })
 
     window.GoogleBook = GoogleBook;
 
 })(window, undefined);
-
-
-    
-        //https://www.googleapis.com/books/v1/volumes?q=search+terms:keyes&key=AIzaSyBU9KgSbBKQMno6QgEoB75TPSRN1c16fLI
-    
-    // GoogleBook.prototype.createInputObject = function(){
-
-    //     var input = {};
-    //     $(':input').each(function(){
-    //         input[this.name] = this.value;
-    //     });
-    //     console.log(input);
-    //     return input;
-    // }
-
-    // GoogleBook.prototype.queryAPI = function() {
-        
-    //     var input = this.createInputObject();
-
-    //     var url = [
-    //         "https://www.googleapis.com/books/v1/volumes",
-    //         "?q=",
-    //         input.genre,
-    //         "&key=",
-    //         this.options.key,
-    //     ].join('');
-        
-    //     console.log(url);
-        
-    //     return $.get(url).then(function(data) {
-    //         console.log(data);
-    //         return data;
-    //     });
-    // };
-    
-    // GoogleBook.prototype.makeGoogleBookRequest = function(data) {
-    //     $.when(
-    //         this.queryAPI(data)
-    //     ).then(function(data) {
-    //         console.log(data);
-    //         //debugger;
-    //         if (!(data.items instanceof Array)
-
-    //         ) {
-    //             throw new Error("Shit Doesn't Work!");
-    //         }
-    //         data.items.forEach(function(data) {
-    //             new containerView({ model: data});
-    //         });
-    //     });
-    // };
-    
-
